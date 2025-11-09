5c82226466ae4573504c215290543da9087c8804
marat_FMK <kosjachok8@yandex.ru>
2025-11-10 01:02:24 +0300

    translater api

--- YaAPI/YaAPI/ContentView.swift
+++ YaAPI/YaAPI/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!") // start project
+            //day 2 first api translater 
         }
         .padding()
     }
